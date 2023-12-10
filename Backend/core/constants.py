39504d25c8652f31de180143ce15f7f96378a6a1
HeshamMoawad <heshammowad120120@yahoo.com>
2023-12-10 11:18:42 +0200

    add constants file

---
+++ Backend/core/constants.py
@@ -0,0 +1,31 @@
+
+
+AUTHORIZATION = "Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs=1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA"
+
+
+ACCEPT_LANGUAGE = "en-US,en;q=0.9"
+
+
+ACCEPT_ENCODING = "gzip, deflate, br"
+
+
+ACCEPT = "*/*"
+
+
+CONTENT_TYPE = "application/json"
+
+
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
+
+
+
+X_TWITTER_ACTIVE_USER = 'yes'
+
+
+X_TWITTER_AUTH_TYPE = 'OAuth2Session'
+
+
+X_TWITTER_CLIENT_LANGUAGE = "en"
+
+
+
