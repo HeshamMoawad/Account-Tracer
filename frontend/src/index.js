2e4c2bb9600b105b675c30d3d83c98d3db80b235
HeshamMoawad <heshammoawad120120@yahoo.com>
2023-12-20 14:30:02 +0200

    add min size of body

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,30 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {BrowserRouter , Route , Routes  } from 'react-router-dom';
-import Home from './components/Home/Home';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Nav from './components/Nav/Nav';
-import Footer from './components/Footer/Footer';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Nav from "./components/Nav/Nav";
+import Footer from "./components/Footer/Footer";
+// import Twitter from "./components/Twitter/Twitter";
+import AgentsPage from "./components/Twitter/AgentsPage/AgentsPage";
+import './index.css'
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 export default function App() {
   return (
-    
     <BrowserRouter>
-      <Nav/>
-      <div>
-      <Routes>
-          <Route index element={<Home />} />
-      </Routes>
-      </div>
-      <Footer/>
+      <Nav />
+        <div id="body">
+        <Routes>
+        <Route index element={<Home />} />
+        {/* <Route path="/twitter" element={<Twitter />} /> */}
+        <Route path="/twitter" element={<AgentsPage />} />
+        </Routes>
+        </div>
+      <Footer />
     </BrowserRouter>
   );
 }
 
 root.render(<App />);
-
-
-
