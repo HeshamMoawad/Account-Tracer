aa712f357dc414495b99df46725ce386631b2d18
HeshamMoawad <heshammoawad120120@yahoo.com>
2023-12-20 14:27:41 +0200

    file that will contain all endpoints

---
+++ frontend/src/Constants.js
@@ -0,0 +1,7 @@
+const BASE_URL = "http://192.168.3.56:8000/";
+
+const TWITTER_GET_ACCOUNTS_URL = BASE_URL + "twitter/api/getAccounts";
+
+const Testing = BASE_URL + "twitter/api/checkExistAccountFViews";
+
+export { BASE_URL, TWITTER_GET_ACCOUNTS_URL, Testing };
