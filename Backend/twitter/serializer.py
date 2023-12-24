6154d8929825a0d84e27c518e8d30554d825cad1
HeshamMoawad <heshammowad120120@yahoo.com>
2023-12-24 14:09:23 +0200

    rm serial add agent , project serial with modified

--- Backend/twitter/serializer.py
+++ Backend/twitter/serializer.py
@@ -1,17 +1,36 @@
-from .models import (AccountRecord , TwitterAccount)
+from .models import (
+    # AccountRecord ,
+    # TwitterAccount , 
+    Agent , 
+    Project
+    )
 from rest_framework.serializers import ModelSerializer
 
 
-class TwitterAccountSerializer(ModelSerializer):
+
+class ProjectSerializer(ModelSerializer):
+
     class Meta:
-        model = TwitterAccount
-        fields = '__all__'
+        model = Project
+        fields = ['name','imgURL','color']
 
 
 
+class AgentSerializer(ModelSerializer):
+    project = ProjectSerializer()
 
-class AccountRecordSerializer(ModelSerializer):
     class Meta:
-        model = AccountRecord
-        fields = '__all__'
+        model = Agent
+        fields = ['name','project']
+
+# class TwitterAccountSerializer(ModelSerializer):
+#     class Meta:
+#         model = TwitterAccount
+#         fields = '__all__'
+
+
+# class AccountRecordSerializer(ModelSerializer):
+#     class Meta:
+#         model = AccountRecord
+#         fields = '__all__'
 
