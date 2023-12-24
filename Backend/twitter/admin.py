5423ab13fd61b1a281606567388f5a774723642c
HeshamMoawad <heshammowad120120@yahoo.com>
2023-12-24 14:15:55 +0200

    add project and agent to admin panel

--- Backend/twitter/admin.py
+++ Backend/twitter/admin.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
 from .models import (
-    TwitterAccount ,
-    AccountRecord
+    # TwitterAccount ,
+    # AccountRecord ,
+    Project ,
+    Agent
     )
 
 # Register your models here.
 
-
-admin.site.register(TwitterAccount)
-admin.site.register(AccountRecord)
+admin.site.register(Project)
+admin.site.register(Agent)
+# admin.site.register(TwitterAccount)
+# admin.site.register(AccountRecord)
 
 
