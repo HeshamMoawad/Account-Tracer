92cc106c6495d3d9e62f526d90d2254d90eb9829
HeshamMoawad <heshammowad120120@yahoo.com>
2023-12-24 14:04:08 +0200

    update setting

--- Backend/accountTracking/settings.py
+++ Backend/accountTracking/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g6ton6l%pg5x99ido*4s!)3@j%&j6%!--s-2=otsh38=d$epu%'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG' , default= False , cast = bool)
 
-ALLOWED_HOSTS = ['192.168.3.53']
+ALLOWED_HOSTS = [config('ALLOWED_HOSTS')]
 
 
 # Application definition
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    'colorfield'
 ]
 
 THIRD_PARTY_APPS = [
@@ -141,9 +144,4 @@ REST_FRAMEWORK = {
 
 
 
-CORS_ALLOWED_ORIGINS=[
-    'http://192.168.3.68:3000' , 
-    'https://twitter.com' , 
-    'chrome://new-tab-page' ,
-]
-
+CORS_ALLOWED_ORIGINS = [config("CORS_ALLOWED_ORIGINS")]
