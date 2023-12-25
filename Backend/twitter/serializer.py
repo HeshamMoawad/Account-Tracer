cd549a2d79bb5f169530a720ccf6a9a77f4ef282
HeshamMoawad <heshammowad120120@yahoo.com>
2023-12-25 12:10:49 +0200

    re shape agent serializer to get requirements

--- Backend/twitter/serializer.py
+++ Backend/twitter/serializer.py
@@ -1,36 +1,38 @@
 from .models import (
-    # AccountRecord ,
-    # TwitterAccount , 
+    TwitterAccount , 
     Agent , 
-    Project
+    Project ,
     )
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer , SerializerMethodField
 
 
 
 class ProjectSerializer(ModelSerializer):
-
     class Meta:
         model = Project
         fields = ['name','imgURL','color']
 
 
 
+class TwitterAccountSerializer(ModelSerializer):
+    class Meta:
+        model = TwitterAccount
+        fields = ['handle','password']
+
+
+
 class AgentSerializer(ModelSerializer):
+    accounts_count = SerializerMethodField()
     project = ProjectSerializer()
+
+    def get_accounts_count(self,obj):
+        return TwitterAccount.objects.filter(agent=obj).count()
 
     class Meta:
         model = Agent
-        fields = ['name','project']
-
-# class TwitterAccountSerializer(ModelSerializer):
-#     class Meta:
-#         model = TwitterAccount
-#         fields = '__all__'
+        fields = ['name','project','accounts_count']
 
 
-# class AccountRecordSerializer(ModelSerializer):
-#     class Meta:
-#         model = AccountRecord
-#         fields = '__all__'
+
+
 
