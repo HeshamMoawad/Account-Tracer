95a4ea041e1370d26022179e40098be12e5be1f4
HeshamMoawad <heshammoawad120120@yahoo.com>
2023-12-28 14:30:07 +0200

    add endpoint connections

--- frontend/src/components/Twitter/AgentsPage/AgentCard/AgentCard.js
+++ frontend/src/components/Twitter/AgentsPage/AgentCard/AgentCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Navigate } from 'react-router-dom';
 import './AgentCard.css';
 
 const AgentEx = {
@@ -12,10 +13,10 @@ const AgentCard = (props) => {
     return (
         <div className='m-auto col-xs-1 col-lg-4 col-md-4' id='card-container'>  
             <div className="card mb-3 mx-auto shadow-lg text-center" id='card'>
-            <div className="card-header h5">{agent.name} - {agent.project}</div>
+            <div className="card-header h5">{agent.name} - {agent.project.name}</div>
             <div className="card-body">
-                <h5 className="card-title mb-3">Account : {agent.account_count}</h5>
-                <a href="##" className="btn btn-primary">
+                <h5 className="card-title mb-3">Account : {agent.accounts_count}</h5>
+                <a href={ `twitter/${agent.project.name}/${agent.name}`} className="btn btn-primary">
                 Go to {agent.name} Accounts
                 </a>
             </div>
--- frontend/src/components/Twitter/AgentsPage/AgentsPage.js
+++ frontend/src/components/Twitter/AgentsPage/AgentsPage.js
@@ -1,65 +1,57 @@
 import React, { useEffect, useState } from "react";
-import AgentCard, { AgentEx } from "./AgentCard/AgentCard";
+import AgentCard from "./AgentCard/AgentCard";
 import AgentCardLoading from "./Loading/AgentCardLoading";
 import Filter from "./Filter/Filter";
 import axios from "axios";
-import { ProjectsURL } from "../../../Constants";
-
-
-
+import { AgentsURL, ProjectsURL } from "../../../Constants";
 
 const AgentsPage = () => {
   const [agents, setAgents] = useState(null);
+  const [projects, setProjects] = useState([]);
   const [filterProjects, setFilterProjects] = useState([]);
 
   useEffect(() => {
-    axios
+    axios     // endpoint connection here
       .get(ProjectsURL)
-      .then(data=>{console.log(data)})
-      .catch(error=>{console.log(error)})
-    // endpoint connection here
-    setAgents([AgentEx, AgentEx, AgentEx, AgentEx]);
-    console.log(filterProjects)
+      .then((data) => {
+        setProjects(data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    
+  }, []);
+
+  useEffect(() => {
+    const params = (filterProjects ? new URLSearchParams({"filter":filterProjects}).toString(): null )
+    axios     // endpoint connection here
+      .get(AgentsURL+ `?${params}` )
+      .then((data) => {
+        setAgents(data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }, [filterProjects]);
-
-
-  const projects = [
-    {
-      'name' : 'profitway',
-      'imgURL' : 'https://profitway.info/images/logo.svg'
-    },
-    {
-      'name' : 'almamlaka',
-      'imgURL' : 'https://almamlaka.info/images/logo.svg'
-    },
-    {
-      'name' : 'al3alamia',
-      'imgURL' : 'https://al3alamia.info/assets/images/logo.svg'
-    },
-    {
-      'name' : 'trading',
-      'imgURL' : 'https://tradingtasi.com/images/trading/logo.svg'
-    },
-    {
-      'name' : 'dddddd',
-      'imgURL' : 'https://tradingtasi.com/images/trading/logo.svg'
-    },
-  ]
-
-
 
   return (
     <>
       <h1 className="h1 text-center mb-5 text-dark">All Agents Twitter </h1>
       <div className="container">
         <div className="row text-center">
-          <Filter filterProjects={filterProjects} setFilterProjects={setFilterProjects} projects={projects}/>
+          <Filter
+            filterProjects={filterProjects}
+            setFilterProjects={setFilterProjects}
+            projects={projects}
+          />
           <div className="container">
             <div className="row">
               {Array.isArray(agents) ? (
                 agents.length === 0 ? (
                   <div className="alert alert-warning text-center" role="alert">
-                    No Accounts Found
+                    No Accounts Found 
+                    <br/>
+                    Please choose Project
                   </div>
                 ) : (
                   agents.map((agent) => {
