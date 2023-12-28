038b700e94a92abd4d8eb45ae328e25ebff7ea6a
HeshamMoawad <heshammoawad120120@yahoo.com>
2023-12-28 14:27:29 +0200

    replace js files with jsx

---
+++ frontend/src/components/Twitter/AgentsPage/Filter/IconButton/IconButton.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+const IconButton = (props) => {
+  const { item, onClickButtonHandler } = props;
+  const [isHovering, setIsHovering] = useState(false);
+  const [isToggled, setToggled] = useState(false);
+  
+  const mouseToggleHandler = () => {
+    setToggled(!isToggled);
+  };
+  const mouseOverLeaveHandler = () => {
+    setIsHovering(!isHovering);
+  };
+
+  return (
+    <>
+      <img
+        src={item.imgURL}
+        alt={item.name}
+        type="button"
+        className="icon-button"
+        id="filter-btn"
+        value={item.name}
+        onClick={(e) => {
+          mouseToggleHandler();
+          onClickButtonHandler(item.name, e);
+        }}
+        style={{
+          width: "80px",
+          height: "70px",
+          maxHeight: "70px",
+          margin: isHovering ? "0px 12px 12px 12px" : "12px",
+          padding: "6px",
+          borderRadius: "50%",
+          backgroundColor: isToggled
+            ? item.color
+            : isHovering
+            ? item.color
+            : "black",
+        }}
+        onMouseEnter={mouseOverLeaveHandler}
+        onMouseLeave={mouseOverLeaveHandler}
+      />
+    </>
+  );
+};
+
+export default IconButton;
