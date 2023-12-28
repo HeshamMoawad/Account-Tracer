ec1691d54190dddead86270e96e9ea981b18004e
HeshamMoawad <heshammoawad120120@yahoo.com>
2023-12-28 14:29:16 +0200

    add new urls (checkexist , accounts)

--- frontend/src/Constants.js
+++ frontend/src/Constants.js
@@ -2,16 +2,19 @@ const BASE_URL = "http://192.168.3.56:8000/";
 
 const TWITTER_GET_ACCOUNTS_URL = BASE_URL + "twitter/api/getAccounts";
 
-const Testing = BASE_URL + "twitter/api/checkExistAccountFViews";
+const CheckExistAccountURL = BASE_URL + "twitter/api/checkExistAccountFBViews";
 
 const ProjectsURL = BASE_URL + "twitter/api/projects";
 
 const AgentsURL = BASE_URL + "twitter/api/agents";
 
+const AccountsURL = BASE_URL + "twitter/api/accounts"
+
 export { 
     BASE_URL, 
     TWITTER_GET_ACCOUNTS_URL, 
-    Testing ,
+    CheckExistAccountURL ,
     ProjectsURL ,
     AgentsURL ,
+    AccountsURL ,
 };
