27eec4ddaac689b1b3eff609585791c0be8244dd
HeshamMoawad <heshammowad120120@yahoo.com>
2023-12-31 15:57:19 +0200

    add AccountLoginInfoListView path

--- Backend/twitter/urls.py
+++ Backend/twitter/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path ,include
+from django.urls import path 
 from .views import (
     checkExistAccountFBViews ,
     ProjectListView ,
     AgentListView ,
+    AccountLoginInfoListView ,
     )
 
 
@@ -10,5 +11,6 @@ urlpatterns = [
     path('api/checkExistAccountFBViews',checkExistAccountFBViews) ,
     path("api/projects" , ProjectListView.as_view()) ,
     path("api/agents" , AgentListView.as_view()) ,
+    path("api/accounts" , AccountLoginInfoListView.as_view()),
 ] 
 
