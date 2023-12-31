cfd0a41cfdc9354e099dc42e951481f58933dd19
HeshamMoawad <heshammowad120120@yahoo.com>
2023-12-31 15:57:06 +0200

    add AccountLoginInfoSerializer and specify fields

--- Backend/twitter/serializer.py
+++ Backend/twitter/serializer.py
@@ -1,38 +1,52 @@
 from .models import (
-    TwitterAccount , 
-    Agent , 
-    Project ,
-    )
-from rest_framework.serializers import ModelSerializer , SerializerMethodField
-
+    TwitterAccount,
+    Agent,
+    Project,
+    AccountLoginInfo
+)
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 
 class ProjectSerializer(ModelSerializer):
     class Meta:
         model = Project
-        fields = ['name','imgURL','color']
-
+        fields = ['name', 'imgURL', 'color']
 
 
 class TwitterAccountSerializer(ModelSerializer):
     class Meta:
         model = TwitterAccount
-        fields = ['handle','password']
-
+        fields = ['handle']  # ,'password'
 
 
 class AgentSerializer(ModelSerializer):
     accounts_count = SerializerMethodField()
     project = ProjectSerializer()
 
-    def get_accounts_count(self,obj):
+    def get_accounts_count(self, obj):
         return TwitterAccount.objects.filter(agent=obj).count()
 
     class Meta:
         model = Agent
-        fields = ['name','project','accounts_count']
+        fields = ['name', 'project', 'accounts_count']
 
 
+class AccountLoginInfoSerializer(ModelSerializer):
+    account = TwitterAccountSerializer()
 
-
-
+    class Meta:
+        model = AccountLoginInfo
+        fields = [
+            'id_str',
+            'name', 
+            'description', 
+            'profileImgURL', 
+            'cookies',
+            'token',
+            'verified', 
+            'suspend',
+            'account', 
+            'created_at', 
+            'created_datetime', 
+            'updated_datetime' ,
+            ]
