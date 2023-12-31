f3c13d20f3a31a24ff5a0bfc5cbc497bfcd6a6bb
HeshamMoawad <heshammoawad120120@yahoo.com>
2023-12-31 16:01:00 +0200

    add main card for showing info

--- frontend/src/components/Twitter/AnalyticsPage/AnalyticsPage.jsx
+++ frontend/src/components/Twitter/AnalyticsPage/AnalyticsPage.jsx
@@ -1,33 +1,83 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import LineInfo from "./LineInfo/LineInfo";
+
+import logo from "../../../assets/facebook.png";
 
 const AnalyticsPage = () => {
-    const {handle} = useParams();
-    const [isExist , setIsExist] = useState(false);
+    const { handle } = useParams();
+    const [isExist, setIsExist] = useState(false);
 
-    // for chack exist handle 
-    useEffect(()=>{
+    // for chack exist handle
+    useEffect(() => {
         // endpoint here
     });
 
     // fetch account info
-    useEffect(()=>{
-
+    useEffect(() => {
+        // endpoint here
     });
 
-
     return (
-        <div className='container text-left '>
-            <div className='row'>
-                <div className="card mx-auto shadow-lg"  style={{ width: "18rem" , minWidth:"1000px" }}>
+        <div className="container text-left ">
+            <div className="row">
+                <div
+                    className="card mx-auto shadow-lg"
+                    style={{ minWidth: "400px" }}
+                >
                     <div className="card-body">
-                        <h5 className="card-title text-center h1">{isExist ? handle : "Not Found " + handle }</h5>
+                        <div className="container text-center">
+                            {isExist ? (
+                                <img className="img-fluid" src={logo} alt="" />
+                            ) : null}
+                            <h1 className="card-title h1">
+                                {isExist ? handle : "Not Found " + handle}
+                            </h1>
+                            <br />
+                        </div>
+                        <div className="container">
+                            <LineInfo
+                                title="Tweets"
+                                value={null}
+                                hasButton={true}
+                                key={Math.random()}
+                            />
+                            <LineInfo
+                                title="Replies"
+                                value={null}
+                                hasButton={true}
+                                key={Math.random()}
+                            />
+                            <LineInfo
+                                title="Follow"
+                                value={null}
+                                key={Math.random()}
+                            />
+                            <LineInfo
+                                title="UnFollow"
+                                value={null}
+                                key={Math.random()}
+                            />
+                            <LineInfo
+                                title="Messages"
+                                value={null}
+                                key={Math.random()}
+                            />
+                            <LineInfo
+                                title="Tweets & Replies"
+                                value={null}
+                                key={Math.random()}
+                            />
+                        </div>
 
+                        {/* <div className="container text-center">
+              <button className="btn btn-secondary">Refresh</button>
+            </div> */}
                     </div>
                 </div>
             </div>
         </div>
     );
-}
+};
 
 export default AnalyticsPage;
