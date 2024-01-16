b2436259557eaed477d5070b5262406fbfdce3d1
HeshamMoawad <heshammowad120120@yahoo.com>
2024-01-16 16:13:54 +0200

    :hammer: rename func with some customize

--- Backend/twitter/core/session.py
+++ Backend/twitter/core/session.py
@@ -1,46 +1,59 @@
 from .abstract import TwitterBaseSession
+from .objects import UserObject
 from ..models import (
-    Chat, 
-    AccountLoginInfo , )
-import typing
-import datetime 
+    Chat,
+    FollowUnFollow,
+)
+import datetime
 
 
 class TwitterSession(TwitterBaseSession):
 
-    def __init__(self, cookies: str , max_older : datetime.datetime = datetime.datetime.now()-datetime.timedelta(days=-2)) -> None:
-        super().__init__(cookies)
-        self.account_model = AccountLoginInfo.objects.filter(rest_id=self.parser.userID).first().account
-        self.max_older = max_older
+    def saveMyStats(self):
+        result = self._getMe()
+        if result:
+            users = result.get("data", {}).get("users", [])
+            if users:
+                user = UserObject(**users[0].get("result"))
+                record = FollowUnFollow.objects.create(
+                    account=self.account_model,
+                    followers=user.followers,
+                    following=user.following,
+                    likes=user.likes
+                )
+                record.save()
+                self.login_info_model.name = user['name']
+                self.login_info_model.profileImgURL = user['profileImgURL']
+                self.login_info_model.description = user['description']
+                self.login_info_model.verified = user['verified']
+                self.login_info_model.save()
 
-    def getMe(self): ...
-
-    def saveMyTweets(self, _cursor=None, _con = False ):
+    def saveMyTweets(self, _cursor=None, _con=False):
         if _cursor or not _con:
-            tweet_parser = self.__getMyTweets(cursor=_cursor)
-            if tweet_parser :
+            tweet_parser = self._getMyTweets(cursor=_cursor)
+            if tweet_parser:
                 tweets = tweet_parser.tweets
                 replies = tweet_parser.replies
-                [self.__saveTweet(tweet) for tweet in tweets]
-                [self.__saveReply(reply) for reply in replies]
-                print(_cursor ,tweets[-1].created_at.date() , self.max_older.date() , tweets[-1].created_at.date() <= self.max_older.date() )
-                if not tweets[-1].created_at.date() <= self.max_older.date() :
-                    _cursor = tweet_parser.getValueFromCursor(tweet_parser.bottomCursor)
+                [self._saveTweet(tweet) for tweet in tweets]
+                [self._saveReply(reply) for reply in replies]
+                # print(_cursor ,tweets[-1].created_at.date() , self.max_older.date() , tweets[-1].created_at.date() <= self.max_older.date() )
+                if not tweets[-1].created_at.date() <= self.max_older.date():
+                    _cursor = tweet_parser.getValueFromCursor(
+                        tweet_parser.bottomCursor)
                     self.saveMyTweets(
-                        _cursor=_cursor ,
+                        _cursor=_cursor,
                         _con=True
                     )
 
-    def saveMyChats(self, initial=None, con=False ):
+    def saveMyChats(self, initial=None, con=False):
         if not initial and not con:
-            initial = self.__init_Inbox()
+            initial = self._init_Inbox()
         if initial:
-            self.__saveObjects(Chat,initial.conversations)
+            self._saveObjects(Chat, initial.conversations)
             if initial.next_entry_id:
-                trusted = self.__trusted(next_entry_id=initial.next_entry_id)
+                trusted = self._trusted(next_entry_id=initial.next_entry_id)
                 if trusted:
-                    self.__saveObjects(Chat,trusted.conversations)
+                    self._saveObjects(Chat, trusted.conversations)
                     if not trusted.conversations[-1].chat_datetime.date() <= self.max_older.date():
                         if trusted.next_entry_id:
                             self.saveMyChats(trusted, con=True)
-
