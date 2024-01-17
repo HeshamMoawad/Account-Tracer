8c90114325f4a3c1968947146d7da4a78e3e7a40
HeshamMoawad <heshammowad120120@yahoo.com>
2024-01-17 16:12:15 +0200

    :sparkles: add periodic tasks BackgroundScheduler

--- Backend/twitter/apps.py
+++ Backend/twitter/apps.py
@@ -1,11 +1,19 @@
 from django.apps import AppConfig
+from apscheduler.schedulers.background import BackgroundScheduler
 
 
 class TwitterConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'twitter'
-
     def ready(self) -> None:
+        from .workers.tasks import (
+            collectorPeriodicTask ,
+            accountCheckerPeriodicTask ,
+            )
+        scheduler = BackgroundScheduler() # ( seconds - minutes - hours )
+        scheduler.add_job(collectorPeriodicTask, 'interval', minutes = 3  )  
+        scheduler.add_job(accountCheckerPeriodicTask, 'interval', minutes = 6 )  
+        scheduler.start()
         from .signals import (
             addAccountLoginInfoBeforeSaveTwitterAccount ,
             )
