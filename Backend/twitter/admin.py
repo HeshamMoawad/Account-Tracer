a1300f33f27124f7299fb059246203a220ddf18b
HeshamMoawad <heshammowad120120@yahoo.com>
2024-01-22 15:57:42 +0200

    :zap: add admin filters

--- Backend/twitter/admin.py
+++ Backend/twitter/admin.py
@@ -13,15 +13,26 @@ from .models import (
     )
 
 # Register your models here.
+class ChatAdmin(admin.ModelAdmin):
+    list_filter = ( "chat_datetime" , "account")
+
+class TweetsAdmin(admin.ModelAdmin):
+    list_filter = ( "account" , "created_at")
+
+class RepliesAdmin(admin.ModelAdmin):
+    list_filter = ( "account" , "created_at")
+
+class FollowUnFollowAdmin(admin.ModelAdmin):
+    list_filter = ( "account" , "created_datetime")
 
 admin.site.register(Project)
 admin.site.register(Agent)
 admin.site.register(TwitterAccount)
 admin.site.register(AccountLoginInfo)
-admin.site.register(Tweet)
-admin.site.register(Chat)
-admin.site.register(Reply)
-admin.site.register(FollowUnFollow)
+admin.site.register(Tweet , TweetsAdmin )
+admin.site.register(Chat , ChatAdmin)
+admin.site.register(Reply , RepliesAdmin)
+admin.site.register(FollowUnFollow , FollowUnFollowAdmin)
 admin.site.register(MediaLink)
 admin.site.register(TelegramBotUser)
 
