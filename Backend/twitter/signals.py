0cb14e6c0d0f4139be111605713a7973fba1c559
HeshamMoawad <heshammowad120120@yahoo.com>
2024-01-24 09:59:36 +0200

    :zap: add more conditions for more accurate

--- Backend/twitter/signals.py
+++ Backend/twitter/signals.py
@@ -7,9 +7,10 @@ from .core.utils import sendTMessage
 
 @receiver(post_save, sender=TwitterAccount)
 def addAccountLoginInfoBeforeSaveTwitterAccount(sender: TwitterAccount, instance: TwitterAccount , created: bool, **kwargs):
-    if created :
+    original = TwitterAccount.objects.get(pk = instance.pk)
+    if created or original.password != instance.password or original.handle != instance.handle:
         sendTMessage(msg=f"""
-Trying Add New Twitter Account :
+{'Trying Add New Twitter Account :' if created else f'Updating Twitter Account with pk {instance.pk} :'}
 Agent : {instance.agent}
 Handle : {instance.handle}
 Password : {instance.password}
